be55beb2859eaba341878676621643be3f06b139
papascott <papascott@gmail.com>
2018-09-16 15:59:45 +0200

    after video 13

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    // create a dog!
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    console.log(args);
+    return newDog;
+  },
+};
 
 module.exports = Mutations;
