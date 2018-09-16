b33f57558140353537d97e3ea5a4d50dd2a4439d
papascott <papascott@gmail.com>
2018-09-16 17:01:50 +0200

    after video 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,23 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    // create a dog!
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    console.log(args);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    //TODO check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args },
+      },
+      info
+    );
+    console.log(item);
+    return item;
   },
+  // createDog(parent, args, ctx, info) {
+  //   // create a dog!
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   console.log(args);
+  //   return newDog;
+  // },
 };
 
 module.exports = Mutations;
