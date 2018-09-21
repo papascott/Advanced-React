034b95b988490f76c90cc0c4e6b02096c83543fa
papascott <papascott@gmail.com>
2018-09-21 15:00:23 +0200

    after video 24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO check if they are logged in
@@ -36,6 +39,32 @@ const Mutations = {
     // TODO
     //3. delete it!(
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parents, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
+    });
+    // finally return user to the browser
+    return user;
   },
 };
 
