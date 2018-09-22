6a1b344e3dc327266ebdfe0439abf52a12275e7b
papascott <papascott@gmail.com>
2018-09-22 18:04:30 +0200

    after video 27

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -66,6 +66,28 @@ const Mutations = {
     // finally return user to the browser
     return user;
   },
+
+  async signin(parents, { email, password }, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid Password!');
+    }
+    // 3. generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
+    });
+    // 5. Return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
