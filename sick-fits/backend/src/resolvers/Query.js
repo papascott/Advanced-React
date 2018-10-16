c3eee95640f417c346a93000fffc46970cfff6a8
papascott <papascott@gmail.com>
2018-10-16 20:48:09 +0200

    after video 52

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -26,6 +26,28 @@ const Query = {
     // 3. If they do, query all the users
     return ctx.db.query.users({}, info);
   },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) throw new Error('You are not logged in!');
+    // 2. Queery the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+
+    // 3. Check if they have permission to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      'ADMIN'
+    );
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd!');
+    }
+    // 4. Return the order
+    return order;
+  },
 };
 
 module.exports = Query;
